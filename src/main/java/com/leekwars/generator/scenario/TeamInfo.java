e27f5480d5bcf28baad985380588888173d9bd8b
Pilow <pierre.laupretre@gmail.com>
2019-12-16 23:04:39 +0100

    [teaminfo] Add level

--- src/main/java/com/leekwars/generator/scenario/TeamInfo.java
+++ src/main/java/com/leekwars/generator/scenario/TeamInfo.java
@@ -5,11 +5,13 @@ import com.alibaba.fastjson.JSONObject;
 public class TeamInfo {
 	public int id;
 	public String name = "";
+	public int level;
 
 	public JSONObject toJson() {
 		JSONObject json = new JSONObject();
 		json.put("id", id);
 		json.put("name", name);
+		json.put("level", level);
 		return json;
 	}
 }
