4b3f0d19d9bfee22306e29b6cd3ec69d4016c492
Pilow <pierre.laupretre@gmail.com>
2021-11-16 11:38:13 +0100

    [data] Improve data loading

--- src/main/java/com/leekwars/generator/Data.java
+++ src/main/java/com/leekwars/generator/Data.java
@@ -1,12 +1,10 @@
 package com.leekwars.generator;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -17,6 +15,7 @@ import java.util.List;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.leekwars.util.Logger;
 
 public class Data {
 
@@ -25,83 +24,55 @@ public class Data {
 	public static List<LocalDate> fullmoon = new ArrayList<>();
 
 	public static void checkData(String api) {
-		File weaponsFile = new File("data/weapons.json");
-		if (!weaponsFile.exists()) {
-			Log.i(TAG, "Load weapons from API...");
-			JSONObject weapons = JSON.parseObject(get(api + "weapon/get-all", "")).getJSONObject("weapons");
-			Util.writeFile(weapons.toJSONString(), "data/weapons.json");
+		Logger.log(TAG, "Check api: " + api);
+		// File weaponsFile = new File("data/weapons.json");
+		Log.i(TAG, "Load weapons from API...");
+		JSONObject weapons = JSON.parseObject(get(api + "weapon/get-all", "")).getJSONObject("weapons");
+		Util.writeFile(weapons.toJSONString(), "data/weapons.json");
+
+		// File chipsFile = new File("data/chips.json");
+		Log.i(TAG, "Load chips from API...");
+		JSONObject chips = JSON.parseObject(get(api + "chip/get-all", "")).getJSONObject("chips");
+		Util.writeFile(chips.toJSONString(), "data/chips.json");
+
+		// File summonsFile = new File("data/summons.json");
+		Log.i(TAG, "Load summons from API...");
+		JSONObject summons = JSON.parseObject(get(api + "summon/get-templates", "")).getJSONObject("summon_templates");
+		Util.writeFile(summons.toJSONString(), "data/summons.json");
+
+		// File functionsFile = new File("data/functions.json");
+		Log.i(TAG, "Load functions from API...");
+		JSONObject functions = JSON.parseObject(get(api + "function/operations", ""));
+		Util.writeFile(functions.toJSONString(), "data/functions.json");
+
+		// File fullmoonFile = new File("data/fullmoon.json");
+		Log.i(TAG, "Load fullmoon from API...");
+		var f = JSON.parseArray(get(api + "fight/fullmoon", ""));
+		for (var d : f) {
+			var dateUTC = ZonedDateTime.of(LocalDateTime.parse((String) d), ZoneOffset.UTC);
+			var dateLocal = dateUTC.withZoneSameInstant(ZoneId.systemDefault()).toLocalDate();
+			fullmoon.add(dateLocal);
 		}
-		File chipsFile = new File("data/chips.json");
-		if (!chipsFile.exists()) {
-			Log.i(TAG, "Load chips from API...");
-			JSONObject chips = JSON.parseObject(get(api + "chip/get-all", "")).getJSONObject("chips");
-			Util.writeFile(chips.toJSONString(), "data/chips.json");
-		}
-		File summonsFile = new File("data/summons.json");
-		if (!summonsFile.exists()) {
-			Log.i(TAG, "Load summons from API...");
-			JSONObject summons = JSON.parseObject(get(api + "summon/get-templates", "")).getJSONObject("summon_templates");
-			Util.writeFile(summons.toJSONString(), "data/summons.json");
-		}
-		File functionsFile = new File("data/functions.json");
-		if (!functionsFile.exists()) {
-			Log.i(TAG, "Load functions from API...");
-			JSONObject functions = JSON.parseObject(get(api + "function/operations", ""));
-			Util.writeFile(functions.toJSONString(), "data/functions.json");
-		}
-		File fullmoonFile = new File("data/fullmoon.json");
-		if (!fullmoonFile.exists()) {
-			Log.i(TAG, "Load fullmoon from API...");
-			var f = JSON.parseArray(get(api + "fight/fullmoon", ""));
-			for (var d : f) {
-				var dateUTC = ZonedDateTime.of(LocalDateTime.parse((String) d), ZoneOffset.UTC);
-				var dateLocal = dateUTC.withZoneSameInstant(ZoneId.systemDefault()).toLocalDate();
-				fullmoon.add(dateLocal);
-			}
-			// System.out.println("full moon = " + fullmoon);
-			Util.writeFile(f.toJSONString(), "data/fullmoon.json");
-		}
+		// System.out.println("full moon = " + fullmoon);
+		Util.writeFile(f.toJSONString(), "data/fullmoon.json");
 	}
 
-    private static String get(String path, String urlParameters) {
-		HttpURLConnection connection = null;
+    private static String get(String url, String urlParameters) {
+		Logger.log(TAG, "get " + url);
+
+		var client = HttpClient.newHttpClient();
+
+		var request = HttpRequest.newBuilder(URI.create(url))
+			.header("accept", "application/json")
+			.build();
+
 		try {
-			// Create connection
-			URL url = new URL(path);
-			connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-
-			connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
-			connection.setRequestProperty("Content-Language", "en-US");
-
-			connection.setUseCaches(false);
-			connection.setDoOutput(true);
-
-			// Send request
-			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-			wr.writeBytes(urlParameters);
-			wr.close();
-
-			// Get Response
-			InputStream is = connection.getInputStream();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-			String line;
-			while ((line = rd.readLine()) != null) {
-				response.append(line);
-				response.append('\r');
-			}
-			rd.close();
-			return response.toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		} finally {
-			if (connection != null) {
-				connection.disconnect();
-			}
+			var response = client.send(request, BodyHandlers.ofString());
+			return response.body();
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace(System.out);
 		}
+		return null;
     }
 
 	public static boolean isFullMoon() {
