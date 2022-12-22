78c42ad5ee7941af2ecda1685d212c4dd79dd3c6
Pilow <pierre.laupretre@gmail.com>
2022-12-22 09:28:44 +0100

    [leekscript] Update

--- src/main/java/com/leekwars/generator/test/LocalDbFileSystem.java
+++ src/main/java/com/leekwars/generator/test/LocalDbFileSystem.java
@@ -60,4 +60,10 @@ public class LocalDbFileSystem extends FileSystem {
 
 	}
 
+	@Override
+	public long getFolderTimestamp(Folder folder) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
